2e06bbca30c86fa82d1aac1354d85d9afa9e53a2
Tim Vierboom <tim-vierboom@gmx.de>
2020-02-20 12:30:24 +0100

    Setup the Cocktail, Dose, Ingredient Models and their relationships

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence: true
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+  validates_uniqueness_of :cocktail_id, scope: [:ingredient_id]
+end
