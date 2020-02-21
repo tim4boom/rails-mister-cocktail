3cc2d859879b13eca4878d1394fc0cb1b7cf4612
Tim Vierboom <tim-vierboom@gmx.de>
2020-02-21 11:54:09 +0100

    created a root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
