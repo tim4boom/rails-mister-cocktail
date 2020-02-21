adaf2c03a765917e3a6f29371695d0cafc1bdb91
Tim Vierboom <tim-vierboom@gmx.de>
2020-02-21 12:14:51 +0100

    Added a list of ingredients and cocktails via API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
+require 'open-uri'
+require 'JSON'
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json = open(url).read
+objs = JSON.parse(json)
+objs["drinks"].each do |object|
+  ingredient = object["strIngredient1"]
+  Ingredient.create(name: ingredient)
+end
 
-
-puts 'Creating ingredients...'
-ingredients_attributes = [
-  {
-    name: "Tonic Water"
-  }
-]
-Ingredient.create!(ingredients_attributes)
-puts 'Finished!'
+url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+json = open(url).read
+objs = JSON.parse(json)
+objs["drinks"].each do |object|
+  cocktail = object["strDrink"]
+  image = object["strDrinkThumb"]
+  Cocktail.create(name: cocktail)
+end
