06fb60ae73a31beef8b989b51bdc00d63a67665b
Tim Vierboom <tim-vierboom@gmx.de>
2020-03-18 13:35:28 +0100

    Added more platforms

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,7 +3,7 @@ class CocktailsController < ApplicationController
     if params[:query].present?
       sql_query = " \
         cocktails.name @@ :query \
-        OR ingredients.name @@ :query \
+        OR ingredients.name ILIKE :query \
       "
       @cocktails = Cocktail.joins(:ingredients).where(sql_query, query: "%#{params[:query]}%")
     else
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,11 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   has_one_attached :image
   validates :name, presence: true, uniqueness: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [:name],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
