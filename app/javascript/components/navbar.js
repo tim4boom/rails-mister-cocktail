3786d12f6e6d8d76a13126182f66e460f12eaf7e
Tim Vierboom <tim-vierboom@gmx.de>
2020-03-24 12:15:37 +0100

    Take out social-media buttons from navbar and insert brand name

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-froffy');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 650) {
+      if (window.scrollY >= 40) {
         navbar.classList.add('navbar-froffy-white');
       } else {
         navbar.classList.remove('navbar-froffy-white');
